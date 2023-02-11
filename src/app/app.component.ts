b85dec1463736f8513ebaf3700d8011c49ec8281
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-11 14:19:44 -0500

    Add Comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+// app.component.ts
+// One of the four main files that are created
+
+// This is your main file which will contain the class with any properties,
+// methods. It will also contain any specifications for the file including
+// the template, stylesheet, etc.
+
 import { Component } from '@angular/core';
 
 @Component({
