93989ef0f096f6ae3fd48cb6322f7b05b8b231de
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-11 14:17:52 -0500

    Add Comments

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+// This file is your "entry point" to Angular
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// This is your main "module" (Angular concept) which is getting "bootstrapped" to your browser
+// You can find the specific module getting "bootstrapped" along this file path: ./app/app.module.ts
 import { AppModule } from './app/app.module';
 
 
