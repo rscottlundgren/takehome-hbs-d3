68fe6969975c012f67e2816a3c442e545c5087af
R. Scott Lundgren <rscottlundgren@gmail.com>
2023-02-12 09:47:39 -0500

    Update `app-priorities` With OnInit & Constructor

--- src/app/components/priorities/priorities.component.ts
+++ src/app/components/priorities/priorities.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-priorities',
   templateUrl: './priorities.component.html',
   styleUrls: ['./priorities.component.css']
 })
-export class PrioritiesComponent {
+export class PrioritiesComponent implements OnInit {
+
+  constructor() {  }
+
+  ngOnInit(): void {}
 
 }
